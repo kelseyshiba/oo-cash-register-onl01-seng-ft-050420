35592d5a3df7547f0f7cd101b8e1cb3ab58c3591
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 18:46:52 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,8 +10,8 @@ class CashRegister
   end
 
   def apply_discount
-      @total = discount.to_f/100 * self.total
-     add_item()
+    @total = discount.to_f/100 * self.total
+    add_item.total * discount.to_f/100
   end
 
   def add_item(title, price, quantity = 1)
