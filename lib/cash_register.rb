5fd24c32d2361fc89182390ddeefaa269250d0fb
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 17:34:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+  attr_accessor :total, :discount
+  
+  def initialize(total = 0, *discount)
+    @total = total
+    @discount = discount
+  end
 
+  def apply_discount
+  end
+
+  def add_item(title, price, quantity = 0)
+  end
+  
+  def total
+  end
+  
+  def items
+  end
+
+  def void_last_transaction
+  end
+
+
+
+end
