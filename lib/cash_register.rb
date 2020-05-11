55dc1eb6138ff12d9c993b366467e4f30336b011
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 19:00:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,6 +15,7 @@ class CashRegister
       puts "After the discount, the total comes to $#{@total}."
     else
       puts "There is no discount to apply."
+    end
   end
   # = discount.to_f/100 * self.total
 
